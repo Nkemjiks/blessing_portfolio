0b61f0db81ae65f1f6879a683ef049de81f99b3d
Blessing Mbonu <mbonunkemjika@ymail.com>
2020-05-16 05:33:29 +0100

    updates route and portfolio

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   resources :portfolios
-  get "pages/home"
-  get "pages/about"
-  get "pages/contact"
+  get "about-me", to: "pages#about"
+  get "contact", to: "pages#contact"
   resources :blogs
   root "pages#home"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
