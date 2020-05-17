17328e37965452fe9f2c285f561f0731eec8378f
Blessing Mbonu <mbonunkemjika@ymail.com>
2020-05-17 11:52:42 +0100

    Add nested attributes

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,5 +1,6 @@
 class Portfolio < ApplicationRecord
   has_many :technologies
+  accepts_nested_attributes_for :technologies, reject_if: :reject_technologies, allow_destroy: true
 
   include Placeholder
   validates_presence_of :title, :subtitle, :body, :main_image, :thumb_image
@@ -10,5 +11,9 @@ class Portfolio < ApplicationRecord
   def set_defaults
     self.main_image ||= Placeholder.image_generator(600, 400)
     self.thumb_image ||= Placeholder.image_generator(350, 200)
+  end
+
+  def reject_technologies(attributes)
+    attributes["name"].blank?
   end
 end
